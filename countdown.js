458efc6a7c2e278b7fc8955ccc1876e96dc04ffd
Bev <qofe79@gmail.com>
2014-04-11 17:22:54 -0400

    countdown exercise

--- countdown.js
+++ countdown.js
@@ -1,5 +1,8 @@
 function countdown(seconds){
-  // ...
+  
+var i=seconds;
+  console.log('wait 5 seconds');
+  setTimeout(function() { setInterval(function() { if (i >= 0) console.log(i);i-- },1000);},5000);
 }
 
 countdown(10);
